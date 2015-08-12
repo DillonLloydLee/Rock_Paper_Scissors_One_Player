0be901f3b3aa6de3fe9ed90d037208193e03f7aa
Brian Borealis & Dillon Lloyd Lee <student@epicodus.com>
2015-08-12 14:28:31 -0700

    add random computer answers.

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -4,8 +4,16 @@
         function game($player_one) {
 
             $player_one = strtoupper ( $player_one );
-            $player_two = "";
-            $player_two = strtoupper ( $player_two );
+            $number = rand(1, 3);
+            if ($number == 1) {
+                $player_two = "ROCK";
+            }
+            elseif ($number == 2) {
+                $player_two = "PAPER";
+            }
+            elseif ($number == 3) {
+                $player_two = "SCISSORS";
+            }
 
             if (($player_one != "ROCK") && ($player_one != "PAPER") && ($player_one != "SCISSORS")) {
                 return "Please answer only with 'rock', 'paper', or 'scissors'.";
@@ -14,7 +22,7 @@
                 return "Please answer only with 'rock', 'paper', or 'scissors'.";
             }
             elseif (($player_one == "ROCK") && ($player_two == "PAPER")) {
-                return "Player Two Wins!";
+                return "Computer Wins!";
             }
             elseif (($player_one == "ROCK") && ($player_two == "SCISSORS")) {
                 return "Player One Wins!";
@@ -23,10 +31,10 @@
                 return "Player One Wins!";
             }
             elseif (($player_one == "PAPER") && ($player_two == "SCISSORS")) {
-                return "Player Two Wins!";
+                return "Computer Wins!";
             }
             elseif (($player_one == "SCISSORS") && ($player_two == "ROCK")) {
-                return "Player Two Wins!";
+                return "Computer Wins!";
             }
             elseif (($player_one == "SCISSORS") && ($player_two == "PAPER")) {
                 return "Player One Wins!";
